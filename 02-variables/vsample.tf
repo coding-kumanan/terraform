787a37372365f9033c0ddec18dd7bb0f052e57ff
kumanan <kumananboss@gmail.com>
2023-09-13 20:48:38 +0530

    variable file with more data types

--- 02-variables/vsample.tf
+++ 02-variables/vsample.tf
@@ -9,11 +9,28 @@ variable "sample1" {
   
 }
 
+variable "number" {
+    default = 100
+}
+
+variable "boolen" {
+    default = true
+}
+// Printing the variable 
 output "op" {
     value = var.sample
   
 }
-
+// want to print the variable along with string use bash representation for vriable. 
 output "op1" {
   value = "This is the second variable value ${var.sample1}"
+}
+
+output "number" {
+    value = var.number
+}
+
+output "boolen" {
+    value = var.boolen
+  
 }
