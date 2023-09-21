e7ae253ddcaef78e48296df6ca49050b0fc204cc
kumanan <kumananboss@gmail.com>
2023-09-21 19:25:13 +0530

    update vm.tf file for create security group

--- 03-samplevm/vm.tf
+++ 03-samplevm/vm.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-071bf1e062558b13c"
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "samplevm"
@@ -11,12 +12,27 @@ output "private_ip_address" {
   value = aws_instance.web.private_dns
 }
 
-output "public_ip_address" {
-    value = aws_instance.web.public_ip
-  
-}
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
 
-output "name" {
-    value = aws_instance.web.security_groups
-  
+  ingress {
+    description      = "SSH From Internet"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
 }
