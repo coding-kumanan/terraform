f2e8d55570c378307fce92d0e238dee4e7268dcd
kumanan <kumananboss@gmail.com>
2023-09-25 23:18:53 +0530

    using count to create multiple resources and data sources to extract resource values

--- 04-remote-state/main.tf
+++ 04-remote-state/main.tf
@@ -1,7 +1,14 @@
+//data source to extract the data from the sources 
+data "aws_ami" "my-ami" {
+  most_recent      = true
+  name_regex       = "centos_Ansible"
+  owners           = ["self"]
+
+}
 resource "aws_instance" "remote-state" {
 //creting multiple resources using count and count value supplied by variable named instacecount.
     count         = var.InstanceCount
-    ami           = "ami-071bf1e062558b13c"
+    ami           = data.aws_ami.my-ami.image_id
     instance_type = "t3.medium"
 
 tags = {
