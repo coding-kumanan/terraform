669589af39d6f24a468cc98d5abd161d7dc0abd0
kumanan <kumananboss@gmail.com>
2023-09-25 20:23:35 +0530

    using count to create multiple resources

--- 04-remote-state/main.tf
+++ 04-remote-state/main.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "remote-state" {
+//creting multiple resources using count and count value supplied by variable named instacecount.
     count         = var.InstanceCount
     ami           = "ami-071bf1e062558b13c"
     instance_type = "t3.medium"
@@ -8,7 +9,6 @@ tags = {
 }
 }
 
-output "instance_id" {
-    value = aws_instance.remote-state["count.index"].id
-  
+output "name" {
+  value = aws_instance.remote-state["${count.index}"].id
 }
