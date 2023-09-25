a84335079706902cf63e3d9d5998e28f0a3f7750
kumanan <kumananboss@gmail.com>
2023-09-26 01:32:44 +0530

    modules in the main file

--- 05-modules/ec2/main.tf
+++ 05-modules/ec2/main.tf
@@ -3,6 +3,10 @@ resource "aws_instance" "moduleinstance" {
     instance_type = "t3.medium"
     vpc_security_group_ids = [var.module_sggroup_variable]
   
+
+tags = {
+    name = "module_instance"
+}
 }
 // declaring empty variable to pass the value of sggroupid from modules main.tf
 variable "module_sggroup_variable" {}
